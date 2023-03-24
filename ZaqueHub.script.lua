3ac8a234f6d908a3450147efd6df12b227030717
0WINDYCAKETI <112848663+0WINDYCAKETI@users.noreply.github.com>
2023-03-24 13:02:48 +0700

    Create ZaqueHub.script.lua

---
+++ ZaqueHub.script.lua
@@ -0,0 +1,7 @@
+--Zaque HUB--
+
+local delay = 0 -- [sec] if your network so noob
+
+repeat wait() wait(delay) until game:IsLoaded()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Mei2232/ZaqueHub/main/Zaque%20Hub"))()
