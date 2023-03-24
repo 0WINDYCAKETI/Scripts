ef2f9eb8dee110ff595c9260426d5630d8146ab3
0WINDYCAKETI <112848663+0WINDYCAKETI@users.noreply.github.com>
2023-03-24 13:03:55 +0700

    Create Mukuro.script.lua

---
+++ Mukuro.script.lua
@@ -0,0 +1,7 @@
+--Mukuro V2--
+
+local delay = 0 -- [Thời gian] nếu như mạng của bạn quá gà
+
+repeat wait() wait(delay) until game:IsLoaded()
+
+loadstring(game:HttpGet"https://raw.githubusercontent.com/xDepressionx/Free-Script/main/AllScript.lua")()
