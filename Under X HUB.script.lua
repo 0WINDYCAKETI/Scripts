f7cf9b5e36c2c65811809ed8adc3f20720e77664
0WINDYCAKETI <112848663+0WINDYCAKETI@users.noreply.github.com>
2023-03-29 18:48:38 +0700

    Update Under X HUB.script.lua

--- Under X HUB.script.lua
+++ Under X HUB.script.lua
@@ -1,11 +1,1 @@
--- Under X HUB --
-
-local delay = 0 -- / [Thời gian] nếu như wifi của bạn quá gà
-
-repeat wait() wait(delay) until game:IsLoaded()
-
-getgenv().JoinTeam = "Pirate" -- / Hải Tạc
-
-shared.keys = "6a3918e1b0350f925d3fe91493499d9" -- / Chìa Khóa
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xSxyz/Project-Dev-beta/main/Project-005-V2"))()
