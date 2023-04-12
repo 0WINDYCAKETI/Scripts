4b68844e23dfb8065552e1b9d4551d04a9e335d1
0Windycaketi <112848663+0WINDYCAKETI@users.noreply.github.com>
2023-04-12 21:30:14 +0700

    Update ZaqueHub.script.lua

--- ZaqueHub.script.lua
+++ ZaqueHub.script.lua
@@ -4,4 +4,4 @@ local delay = 0 -- [sec] if your network so noob
 
 repeat wait() wait(delay) until game:IsLoaded()
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Mei2232/ZaqueHub/main/Zaque%20Hub"))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/ZaqueHub/BloxFruitPC/main/Protected.lua'))()
