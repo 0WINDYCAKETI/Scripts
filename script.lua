8af6b8d1e52ced18cf129753ebcdbd09bcd0b6e1
0Windycaketi <112848663+0WINDYCAKETI@users.noreply.github.com>
2023-05-30 20:30:25 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,2 +1,4 @@
 --Local Lib--
-local IdkLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local IdkLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/0WINDYCAKETI/Scripts/main/Lib')))()
+--Local MainWindown--
+local Window = OrionLib:MakeWindow({Name = "WatermelonHUB", HidePremium = false, SaveConfig = true, ConfigFolder = "Watermelon", IntroText = "WatermelonLib"})
